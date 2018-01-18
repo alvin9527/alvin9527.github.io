c9eb2217321aa09187e643765c57a903365024e9
alvin <chinaguoxu@163.com>
2018-01-18 14:27:17 +0800

    日期修改
    倒计时延长

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "January 25, 2018 00:00:00",
+	date: "February 15, 2018 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
